cd53e5fbf0d182968ae912503679845625624185
Komal-8080 <shindeg163@gmail.com>
2021-01-04 23:37:04 +0530

    Modified: Methods modified to return as Happy or Sad while performing…

--- main/java/UserRegistrationGradle/UserRegistrationGradle.java
+++ main/java/UserRegistrationGradle/UserRegistrationGradle.java
@@ -5,39 +5,64 @@ import java.util.regex.Pattern;
 
 public class UserRegistrationGradle {
 	
-	public static boolean UC1(String firstNameUserInput) {		
+	public static String UC1(String firstNameUserInput) {		
 		Pattern firstName = Pattern.compile("[A-Z]{1}[a-z]{2,}$");  
 		Matcher match = firstName.matcher(firstNameUserInput);  
 		boolean patternValidity = match.matches();
-		return patternValidity;		
+		if (patternValidity == true) { 
+			return "HAPPY";
+		}
+		else {
+			return "SAD";
+		}	
 	}
 	
-	public static boolean UC2(String lastNameUserInput) {		
+	public static String UC2(String lastNameUserInput) {		
 		Pattern lastName = Pattern.compile("[A-Z]{1}[a-z]{2,}$");  
 		Matcher match = lastName.matcher(lastNameUserInput);  
 		boolean patternValidity = match.matches();
-		return patternValidity;	
+		if (patternValidity == true) { 
+			return "HAPPY";
+		}
+		else {
+			return "SAD";
+		}
 	}
 	
-	public static boolean UC3(String emailUserInput) {
+	public static String UC3(String emailUserInput) {
 		Pattern email = Pattern.compile("^[a-zA-Z]{3}\\.[a-zA-Z]{1,}@[a-zA-Z]{2}\\.[a-zA-Z]{2}\\.[a-zA-Z]{1,}$");  
 		Matcher match = email.matcher(emailUserInput);  
 		boolean patternValidity = match.matches();
-		return patternValidity;
+		if (patternValidity == true) { 
+			return "HAPPY";
+		}
+		else {
+			return "SAD";
+		}
 	}
 
-	public boolean UC4(String mobileUserInput) {
+	public String UC4(String mobileUserInput) {
 		Pattern mobileNumber = Pattern.compile("^[1-9]{2}\\s{1}[0-9]{1}[0-9]{9}$");  
 		Matcher match = mobileNumber.matcher(mobileUserInput);  
 		boolean patternValidity = match.matches();
-		return patternValidity;
+		if (patternValidity == true) { 
+			return "HAPPY";
+		}
+		else {
+			return "SAD";
+		}
 	}	
 	
-	public static boolean UC5toUC8(String passwordUserInput) {
+	public static String UC5toUC8(String passwordUserInput) {
 		Pattern password = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}");  
 		Matcher match = password.matcher(passwordUserInput);  
 		boolean patternValidity = match.matches();
-		return patternValidity;
+		if (patternValidity == true) { 
+			return "HAPPY";
+		}
+		else {
+			return "SAD";
+		}
 	}
 	
 	public static boolean UC9(String sampleEmailValidation) {
--- test/java/UserRegistrationGradle/UserRegistrationGradleTest.java
+++ test/java/UserRegistrationGradle/UserRegistrationGradleTest.java
@@ -6,60 +6,62 @@ import java.util.Scanner;
 
 public class UserRegistrationGradleTest {
 	
+	 static String Happy = null;
+	
 	public static void UC1() {
 		UserRegistrationGradle userRegistrationGradle = new UserRegistrationGradle();
-		boolean firstNameUserInput = userRegistrationGradle.UC1("Komal");
+		String firstNameUserInput = userRegistrationGradle.UC1("Komal");
 		//Assert Statement
-		assertEquals(true, "firstNameUserInput");
+		assertEquals(Happy, firstNameUserInput);
 	}
 	
 	public static void UC2() {
 		UserRegistrationGradle userRegistrationGradle = new UserRegistrationGradle();
-		boolean lastNameUserInput = userRegistrationGradle.UC2("Shinde");
+		String lastNameUserInput = userRegistrationGradle.UC2("Shinde");
 		//Assert Statement
-		assertEquals(true, "lastNameUserInput");
+		assertEquals(Happy, "lastNameUserInput");
 	}
 	
 	public static void UC3() {
 		UserRegistrationGradle userRegistrationGradle = new UserRegistrationGradle();
-		boolean emailUserInput = userRegistrationGradle.UC3("abc.xyz@bl.co.in");
+		String emailUserInput = userRegistrationGradle.UC3("abc.xyz@bl.co.in");
 		//Assert Statement
-		assertEquals(true, "emailUserInput");
+		assertEquals(Happy, "emailUserInput");
 	}
 	
 	public static void UC4() {
 		UserRegistrationGradle userRegistrationGradle = new UserRegistrationGradle();
-		boolean mobileUserInput = userRegistrationGradle.UC4("91 8562416354");
+		String mobileUserInput = userRegistrationGradle.UC4("91 8562416354");
 		//Assert Statement
-		assertEquals(true, "mobileUserInput");
+		assertEquals(Happy, "mobileUserInput");
 	}
 	
 	public static void UC5PasswordRule1() {
 		UserRegistrationGradle userRegistrationGradle = new UserRegistrationGradle();
-		boolean passwordUserInput = userRegistrationGradle.UC5toUC8("Komal");
+		String passwordUserInput = userRegistrationGradle.UC5toUC8("Komal");
 		//Assert Statement
-		assertEquals(true, "passwordUserInput");
+		assertEquals(Happy, "passwordUserInput");
 	}
 	
 	public static void UC6PasswordRule2() {
 		UserRegistrationGradle userRegistrationGradle = new UserRegistrationGradle();
-		boolean passwordUserInput = userRegistrationGradle.UC5toUC8("Komal");
+		String passwordUserInput = userRegistrationGradle.UC5toUC8("Komal");
 		//Assert Statement
-		assertEquals(true, "passwordUserInput");
+		assertEquals(Happy, "passwordUserInput");
 	}
 	
 	public static void UC7PasswordRule3() {
 		UserRegistrationGradle userRegistrationGradle = new UserRegistrationGradle();
-		boolean passwordUserInput = userRegistrationGradle.UC5toUC8("Komal");
+		String passwordUserInput = userRegistrationGradle.UC5toUC8("Komal");
 		//Assert Statement
-		assertEquals(true, "passwordUserInput");
+		assertEquals(Happy, "passwordUserInput");
 	}
 	
 	public static void UC8PasswordRule4() {
 		UserRegistrationGradle userRegistrationGradle = new UserRegistrationGradle();
-		boolean passwordUserInput = userRegistrationGradle.UC5toUC8("Komal");
+		String passwordUserInput = userRegistrationGradle.UC5toUC8("Komal");
 		//Assert Statement
-		assertEquals(true, "passwordUserInput");
+		assertEquals(Happy, "passwordUserInput");
 	}
 	
 	public static void UC9() {
