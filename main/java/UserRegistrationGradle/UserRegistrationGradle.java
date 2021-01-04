7658b7e5b6cebd3d829e1ea2656e24b99777a470
Komal-8080 <shindeg163@gmail.com>
2021-01-04 17:47:26 +0530

    Initialization: Main class and Test Class Initialized

---
+++ main/java/UserRegistrationGradle/UserRegistrationGradle.java
@@ -0,0 +1,8 @@
+package UserRegistrationGradle;
+
+public class UserRegistrationGradle {
+
+	public static void main(String args[]) {
+		System.out.println("UserDetails Validation program");
+	}
+}
---
+++ test/java/UserRegistrationGradle/UserRegistrationTest.java
@@ -0,0 +1,5 @@
+package UserRegistrationGradle;
+
+public class UserRegistrationTest {
+
+}
